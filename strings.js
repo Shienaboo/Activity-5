981e2c4edd9693fae5e649bddc43c79e25b675f3
unknown <nshienarocell@gmail.com>
2024-09-14 22:15:43 -0800

    Initial commit with strings.js

---
+++ strings.js
@@ -0,0 +1,14 @@
+let favActorsFirstName = "Jack";
+let favActorsLastName = "Roberto";
+let fullname = favActorsFirstName + " " + favActorsLastName;
+console.log(favActorsFirstName);
+console.log(favActorsLastName);
+console.log(fullname);
+
+let uppercase = "jack roberto";
+console.log(uppercase.toUpperCase());
+let lowercase = "XYXY";
+console.log(lowercase.toLowerCase());
+
+let message = "My favorite actor is " + (fullname.toUpperCase()) + " his best show is Meant to Be";
+console.log(message);
